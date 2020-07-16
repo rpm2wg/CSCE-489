57c3365b60957d2582053d2e5ddcb2294a97a865
Ryan Montgomery <rpm2wg@gmail.com>
2020-07-15 23:45:10 -0400

    Proj0 Complete

---
+++ hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  //including the standard input output to print the message in the terminal
+
+int main() 
+{
+    printf("Hello, world!\n"); //prints Hello, world! in the terminal then moves to a new line to make it look cleaner
+    return 0; 
+}
